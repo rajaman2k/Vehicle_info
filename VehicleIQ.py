814829e6c73a1346431fb1cc54e67391408cc60f
Al Aman <88160864+rajaman2k@users.noreply.github.com>
2026-03-25 01:20:29 +0530

    Update VehicleIQ.py
    changed mofrl from 1.5 flash to pro

--- VehicleIQ.py
+++ VehicleIQ.py
@@ -223,7 +223,7 @@ except (KeyError, FileNotFoundError):
 @st.cache_resource
 def get_model():
     return ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash",
+        model="gemini-pro",
         api_key=API_KEY,
         temperature=0.6,
     )
